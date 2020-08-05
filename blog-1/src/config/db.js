ac0e2e00775bf45bd96678718754ff1bf2beb40b
weikee94 <weikee070194@gmail.com>
2020-08-05 14:47:35 +0800

    create config n db folder

---
+++ blog-1/src/config/db.js
@@ -0,0 +1,27 @@
+const env = process.env.NODE_ENV; // 环境变量
+
+let MYSQL_CONF;
+
+if (env === "dev") {
+  MYSQL_CONF = {
+    host: "localhost",
+    user: "root",
+    password: "password",
+    port: "3306", // default port
+    database: "myblog",
+  };
+}
+
+if (env === "production") {
+  MYSQL_CONF = {
+    host: "localhost",
+    user: "root",
+    password: "password",
+    port: "3306", // default port
+    database: "myblog",
+  };
+}
+
+module.exports = {
+  MYSQL_CONF,
+};
