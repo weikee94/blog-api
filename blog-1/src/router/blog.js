f7e44160f37c64a62931cedbe61006b5e5703c15
weikee94 <weikee070194@gmail.com>
2020-08-07 11:59:07 +0800

    setup nginx

--- blog-1/src/router/blog.js
+++ blog-1/src/router/blog.js
@@ -7,14 +7,34 @@ const {
 } = require("../controller/blog");
 const { SuccessModel, ErrorModel } = require("../model/resModel");
 
+// 统一的登录验证函数
+const loginCheck = (req) => {
+  if (!req.session.username) {
+    return Promise.resolve(new ErrorModel("尚未登录"));
+  }
+};
+
 const handleBlogRouter = (req, res) => {
   const method = req.method; // GET POST
   const id = req.query.id;
 
   // retrieve blog list
   if (method === "GET" && req.path === "/api/blog/list") {
-    const author = req.query.author || "";
+    let author = req.query.author || "";
     const keyword = req.query.keyword || "";
+
+    if (req.query.isadmin) {
+      // 管理员界面
+      const loginCheckResult = loginCheck(req);
+      if (loginCheckResult) {
+        // 未登录
+        return loginCheckResult;
+      }
+
+      // 强制查询自己的博客
+      author = req.session.username;
+    }
+
     const result = getList(author, keyword);
     return result.then((listData) => {
       return new SuccessModel(listData);
@@ -36,7 +56,13 @@ const handleBlogRouter = (req, res) => {
     // const data = newBlog(req.body);
     // return new SuccessModel(data);
 
-    req.body.author = "zhangsan";
+    const loginCheckResult = loginCheck(req);
+    if (loginCheckResult) {
+      // 未登录
+      return loginCheckResult;
+    }
+
+    req.body.author = req.session.username;
     const result = newBlog(req.body);
     return result.then((data) => {
       return new SuccessModel(data);
@@ -45,6 +71,12 @@ const handleBlogRouter = (req, res) => {
 
   // update a blog
   if (method === "POST" && req.path === "/api/blog/update") {
+    const loginCheckResult = loginCheck(req);
+    if (loginCheckResult) {
+      // 未登录
+      return loginCheckResult;
+    }
+
     const result = updateBlog(id, req.body);
     return result.then((val) => {
       if (val) {
@@ -57,7 +89,13 @@ const handleBlogRouter = (req, res) => {
 
   // delete blog
   if (method === "POST" && req.path === "/api/blog/del") {
-    const author = "author bb"; // fake data
+    const loginCheckResult = loginCheck(req);
+    if (loginCheckResult) {
+      // 未登录
+      return loginCheckResult;
+    }
+
+    const author = req.session.username;
     const result = delBlog(id, author);
     return result.then((val) => {
       if (val) {
