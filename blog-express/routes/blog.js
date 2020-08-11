4b47d4e8ba55e0f4a708ba62dcc15916368140e7
weikee94 <weikee070194@gmail.com>
2020-08-11 15:53:13 +0800

    refine path by express way

--- blog-express/routes/blog.js
+++ blog-express/routes/blog.js
@@ -1,5 +1,4 @@
 var express = require("express");
-const { route } = require(".");
 var router = express.Router();
 const {
   getList,
@@ -9,23 +8,24 @@ const {
   delBlog,
 } = require("../controller/blog");
 const { SuccessModel, ErrorModel } = require("../model/resModel");
+const loginCheck = require("../middleware/loginCheck");
 
 /* GET blog page. */
-router.get("/list", function (req, res, next) {
+router.get("/list", (req, res, next) => {
   let author = req.query.author || "";
   const keyword = req.query.keyword || "";
 
-  // if (req.query.isadmin) {
-  //   // 管理员界面
-  //   const loginCheckResult = loginCheck(req);
-  //   if (loginCheckResult) {
-  //     // 未登录
-  //     return loginCheckResult;
-  //   }
+  if (req.query.isadmin) {
+    // 管理员界面
+    if (req.session.username == null) {
+      // 未登录
+      res.json(new ErrorModel("havent login"));
+      return;
+    }
 
-  //   // 强制查询自己的博客
-  //   author = req.session.username;
-  // }
+    // 强制查询自己的博客
+    author = req.session.username;
+  }
 
   const result = getList(author, keyword);
   return result.then((listData) => {
@@ -34,10 +34,44 @@ router.get("/list", function (req, res, next) {
 });
 
 // Get blog details
-router.get("/detail", function (req, res, next) {
-  res.json({
-    error: 0,
-    data: "okie",
+router.get("/detail", (req, res, next) => {
+  const result = getDetail(req.query.id);
+  return result.then((data) => {
+    res.json(new SuccessModel(data));
+  });
+});
+
+// create new blog
+router.post("/new", loginCheck, (req, res, next) => {
+  req.body.author = req.session.username;
+  const result = newBlog(req.body);
+  return result.then((data) => {
+    res.json(new SuccessModel(data));
+  });
+});
+
+// update a blog
+router.post("/update", loginCheck, (req, res, next) => {
+  const result = updateBlog(req.query.id, req.body);
+  return result.then((val) => {
+    if (val) {
+      res.json(new SuccessModel());
+    } else {
+      res.json(new ErrorModel("update blog failed"));
+    }
+  });
+});
+
+// delete a blog
+router.post("/del", loginCheck, (req, res, next) => {
+  const author = req.session.username;
+  const result = delBlog(req.query.id, author);
+  return result.then((val) => {
+    if (val) {
+      res.json(new SuccessModel());
+    } else {
+      res.json(new ErrorModel("remove blog failed"));
+    }
   });
 });
 
